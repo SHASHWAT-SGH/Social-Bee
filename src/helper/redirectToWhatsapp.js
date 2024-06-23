8d9924d85a53b5e1008fa983a00948da521adec5
SHASHWAT-SGH <vnsshashwat@gmail.com>
2024-06-23 12:11:45 +0530

    changes ui for phone in about-me section

--- src/helper/redirectToWhatsapp.js
+++ src/helper/redirectToWhatsapp.js
@@ -1,6 +1,7 @@
 const redirectToWhatsApp = () => {
   let phoneNumber = "+91 8840234936";
-  let message = "Hello, I would like to know more about your services.";
+  let message =
+    "Hello, I would like to know more about your services related to social media management.";
   let whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
     message
   )}`;
